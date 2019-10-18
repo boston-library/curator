081f473f61c56653d4cc796e38f277cc121812e3
Eben English <eenglish@bpl.org>
2019-10-18 16:36:13 -0400

    update rails_helper so we can run rspec

--- spec/models/curator/mappings/desc_term_spec.rb
+++ spec/models/curator/mappings/desc_term_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 module Curator
-  RSpec.describe Mapping::DescNameRoleMapping, type: :model do
+  RSpec.describe Mappings::DescTerm, type: :model do
     pending "add some examples to (or delete) #{__FILE__}"
   end
 end
