d77fb74e1714641729d4236018355bb41c7623f6
bbarberBPL <bbarber@bpl.org>
2019-10-29 14:34:13 -0400

    Fixed setup rake task to be used from the project root instead of the internal app root

--- lib/curator/engine.rb
+++ lib/curator/engine.rb
@@ -35,8 +35,10 @@ module Curator
     end
 
     initializer 'curator.append_migrations' do |app|
-      config.paths['db/migrate'].expanded.each do |path|
-        app.config.paths['db/migrate'].push(path)
+      unless app.root.to_s.match root.to_s
+        config.paths['db/migrate'].expanded.each do |path|
+          app.config.paths['db/migrate'].push(path)
+        end
       end
     end
 
