74f4a92608cd601685b3eccb05f399448a61ff34
bbarberBPL <bbarber@bpl.org>
2019-11-01 17:06:36 -0400

    A little more setup

--- spec/factories/curator/controlled_terms/geographics.rb
+++ spec/factories/curator/controlled_terms/geographics.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :curator_controlled_terms_geographic, class: 'Curator::ControlledTerms::Geographic' do
     association :authority, factory: :curator_controlled_terms_authority
     term_data { {} }
-    type { "Curator::ControlledTerms::Geographic" }
+    type { 'Curator::ControlledTerms::Geographic' }
     archived_at { nil }
   end
 end
--- spec/models/curator/institution_spec.rb
+++ spec/models/curator/institution_spec.rb
@@ -4,10 +4,9 @@ require 'rails_helper'
 require_relative './shared/mintable.rb'
 module Curator
   RSpec.describe Institution, type: :model do
-    before(:each) do
-      @institution = create(:curator_institution)
-    end
-    subject { @institution }
+    let!(:institution) { create(:curator_institution) }
+
+    subject { institution }
 
     it_behaves_like 'mintable'
   end
