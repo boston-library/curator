88f40abf69cdc75ab810e9258f6a19bc74d14cf5
bbarberBPL <bbarber@bpl.org>
2019-11-01 17:06:36 -0400

    A little more setup

--- spec/models/curator/collection_spec.rb
+++ spec/models/curator/collection_spec.rb
@@ -4,10 +4,9 @@ require 'rails_helper'
 require_relative './shared/mintable.rb'
 module Curator
   RSpec.describe Collection, type: :model do
-    before(:each) do
-      @collection = create(:curator_collection)
-    end
-    subject { @collection }
+    let!(:collection) { create(:curator_collection) }
+
+    subject { collection }
 
     it_behaves_like 'mintable'
   end
