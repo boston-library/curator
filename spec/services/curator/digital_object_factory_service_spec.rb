d83a46b6e932cbfdfa6c5d92a3e984673a9b8029
bbarberBPL <bbarber@bpl.org>
2019-11-04 16:57:42 -0500

    Added a Json fixture loader helper to make life easier

--- spec/services/curator/digital_object_factory_service_spec.rb
+++ spec/services/curator/digital_object_factory_service_spec.rb
@@ -4,16 +4,14 @@ require 'rails_helper'
 require_relative './factory_service_metastreams_shared'
 
 RSpec.describe Curator::DigitalObjectFactoryService do
-  json_fixture = File.join(Curator::Engine.root.join('spec', 'fixtures', 'files', 'digital_object.json'))
-  object_json = JSON.parse(File.read(json_fixture)).fetch('digital_object', {})
-
   before(:all) do
+    @object_json = load_json_fixture('digital_object.json', 'digital_object')
     # create parent Collection
     parent = create(:curator_collection)
-    object_json['admin_set']['ark_id'] = parent.ark_id
-    object_json['is_member_of_collection'][0]['ark_id'] = parent.ark_id
+    @object_json['admin_set']['ark_id'] = parent.ark_id
+    @object_json['is_member_of_collection'][0]['ark_id'] = parent.ark_id
     expect do
-      @object = described_class.call(json_data: object_json)
+      @object = described_class.call(json_data: @object_json)
     end.to change { Curator::DigitalObject.count }.by(1)
   end
 
@@ -21,8 +19,8 @@ RSpec.describe Curator::DigitalObjectFactoryService do
     subject { @object }
 
     it 'has the correct properties' do
-      expect(subject.ark_id).to eq object_json['ark_id']
-      expect(subject.created_at).to eq Time.zone.parse(object_json['created_at'])
+      expect(subject.ark_id).to eq @object_json['ark_id']
+      expect(subject.created_at).to eq Time.zone.parse(@object_json['created_at'])
     end
 
     describe 'setting admin set' do
@@ -43,7 +41,7 @@ RSpec.describe Curator::DigitalObjectFactoryService do
 
     describe 'descriptive metastream' do
       let(:descriptive) { subject.descriptive }
-      let(:desc_json)   { object_json['metastreams']['descriptive'] }
+      let(:desc_json)   { @object_json['metastreams']['descriptive'] }
       let(:simple_fields) do
         %w(abstract access_restrictions digital_origin frequency issuance origin_event extent
            physical_location_department physical_location_shelf_locator place_of_publication
@@ -269,12 +267,12 @@ RSpec.describe Curator::DigitalObjectFactoryService do
       end
     end
 
-    it_behaves_like 'workflowable', object_json
-    it_behaves_like 'administratable', object_json
+    it_behaves_like 'workflowable', @object_json
+    it_behaves_like 'administratable', @object_json
 
     describe 'administrative metastream' do
       let(:administrative) { subject.administrative }
-      let(:administrative_json) { object_json['metastreams']['administrative'] }
+      let(:administrative_json) { @object_json['metastreams']['administrative'] }
       it 'sets the correct administrative metadata' do
         expect(administrative.description_standard).to eq administrative_json['description_standard']
         expect(administrative.flagged).to eq administrative_json['flagged']
--- spec/services/curator/filestreams/file_set_factory_service_spec.rb
+++ spec/services/curator/filestreams/file_set_factory_service_spec.rb
@@ -4,34 +4,32 @@ require 'rails_helper'
 require_relative './../factory_service_metastreams_shared'
 
 RSpec.describe Curator::Filestreams::FileSetFactoryService do
-  json_fixture = File.join(Curator::Engine.root.join('spec', 'fixtures', 'files', 'file_set.json'))
-  object_json = JSON.parse(File.read(json_fixture)).fetch('file_set', {})
-
   before(:all) do
+    @object_json = load_json_fixture('file_set.json', 'file_set')
     # create parent DigitalObject and Collection
     parent_col = create(:curator_collection)
     parent_obj = create(:curator_digital_object)
     parent_obj.workflow = create(:curator_metastreams_workflow)
-    object_json['file_set_of']['ark_id'] = parent_obj.ark_id
-    object_json['exemplary_image_of'][0]['ark_id'] = parent_obj.ark_id
-    object_json['exemplary_image_of'][1]['ark_id'] = parent_col.ark_id
-    object_json['metastreams']['workflow']['publishing_state'] = parent_obj.workflow.publishing_state
+    @object_json['file_set_of']['ark_id'] = parent_obj.ark_id
+    @object_json['exemplary_image_of'][0]['ark_id'] = parent_obj.ark_id
+    @object_json['exemplary_image_of'][1]['ark_id'] = parent_col.ark_id
+    @object_json['metastreams']['workflow']['publishing_state'] = parent_obj.workflow.publishing_state
     expect do
-      @file_set = described_class.call(json_data: object_json)
+      @file_set = described_class.call(json_data: @object_json)
     end.to change { Curator::Filestreams::FileSet.count }.by(1)
   end
 
   describe '#call' do
     subject { @file_set }
 
-    let(:file_set_type) { object_json['file_set_type'] }
+    let(:file_set_type) { @object_json['file_set_type'] }
 
     it 'has the correct properties' do
       %w(ark_id position file_name_base pagination).each do |attr|
-        expect(@file_set.send(attr)).to eq object_json[attr]
+        expect(@file_set.send(attr)).to eq @object_json[attr]
       end
       expect(subject.file_set_type).to eq Curator.filestreams.send("#{file_set_type}_class_name")
-      expect(subject.updated_at).to eq Time.zone.parse(object_json['updated_at'])
+      expect(subject.updated_at).to eq Time.zone.parse(@object_json['updated_at'])
     end
 
     describe 'object relationships' do
@@ -39,7 +37,7 @@ RSpec.describe Curator::Filestreams::FileSetFactoryService do
         let(:digital_object) { @file_set.file_set_of }
         it 'sets the file_set_of relationship' do
           expect(digital_object).to be_an_instance_of(Curator::DigitalObject)
-          expect(digital_object.ark_id).to eq object_json['file_set_of']['ark_id']
+          expect(digital_object.ark_id).to eq @object_json['file_set_of']['ark_id']
         end
       end
 
@@ -47,17 +45,17 @@ RSpec.describe Curator::Filestreams::FileSetFactoryService do
         let(:exemplary_image_object) { @file_set.exemplary_image_objects.first }
         it 'sets the exemplary_image_object relationship' do
           expect(exemplary_image_object).to be_an_instance_of(Curator::DigitalObject)
-          expect(exemplary_image_object.ark_id).to eq object_json['exemplary_image_of'][0]['ark_id']
+          expect(exemplary_image_object.ark_id).to eq @object_json['exemplary_image_of'][0]['ark_id']
         end
 
         let(:exemplary_image_collection) { @file_set.exemplary_image_collections.first }
         it 'sets the exemplary_image_collection relationship' do
           expect(exemplary_image_collection).to be_an_instance_of(Curator::Collection)
-          expect(exemplary_image_collection.ark_id).to eq object_json['exemplary_image_of'][1]['ark_id']
+          expect(exemplary_image_collection.ark_id).to eq @object_json['exemplary_image_of'][1]['ark_id']
         end
       end
     end
 
-    it_behaves_like 'workflowable', object_json
+    it_behaves_like 'workflowable'
   end
 end
