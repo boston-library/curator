a815595f4fd975c0dc7fc007276cf83d73fbe950
bbarberBPL <bbarber@bpl.org>
2019-11-04 12:44:20 -0500

    Base unit tests for institution done

--- spec/models/curator/institution_spec.rb
+++ spec/models/curator/institution_spec.rb
@@ -6,5 +6,24 @@ require_relative './shared/mintable.rb'
 RSpec.describe Curator::Institution, type: :model do
   subject { create(:curator_institution) }
 
+  it { is_expected.to have_db_column(:name).of_type(:string) }
+  it { is_expected.to have_db_column(:url).of_type(:string) }
+  it { is_expected.to have_db_column(:abstract).of_type(:text) }
+
   it_behaves_like 'mintable'
+
+  describe 'Associations' do
+    it { is_expected.to belong_to(:location).
+      inverse_of(:institution_locations).
+      class_name('Curator::ControlledTerms::Geographic').optional }
+
+    it { is_expected.to have_many(:host_collections).
+      inverse_of(:institution).class_name('Curator::Mappings::HostCollection') }
+
+    it { is_expected.to have_many(:collections).
+      inverse_of(:institution).class_name('Curator::Collection').dependent(:destroy) }
+
+    it { is_expected.to have_many(:collection_admin_set_objects).
+      through(:collections).source(:admin_set_objects) }
+  end
 end
--- spec/models/curator/shared/mintable.rb
+++ spec/models/curator/shared/mintable.rb
@@ -2,7 +2,9 @@
 
 RSpec.shared_examples_for 'mintable', type: :model do
   describe '#ark_id' do
-    it { is_expected.to validate_presence_of :ark_id }
-    it { is_expected.to validate_uniqueness_of :ark_id }
+    it { is_expected.to have_db_column(:ark_id).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_index(:ark_id).unique(true) }
+    it { is_expected.to validate_presence_of(:ark_id) }
+    it { is_expected.to validate_uniqueness_of(:ark_id) }
   end
 end
