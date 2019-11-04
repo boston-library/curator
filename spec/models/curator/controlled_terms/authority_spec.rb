15da6547f0937c1a2dafc6a3fe5808427c12381d
bbarberBPL <bbarber@bpl.org>
2019-11-04 11:27:51 -0500

    Set up rubocop rspec for models

--- spec/models/curator/controlled_terms/authority_spec.rb
+++ spec/models/curator/controlled_terms/authority_spec.rb
@@ -2,8 +2,6 @@
 
 require 'rails_helper'
 
-module Curator
-  RSpec.describe ControlledTerms::Authority, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+RSpec.describe Curator::ControlledTerms::Authority, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/curator/metastreams/workflow_spec.rb
+++ spec/models/curator/metastreams/workflow_spec.rb
@@ -2,8 +2,6 @@
 
 require 'rails_helper'
 
-module Curator
-  RSpec.describe Metastreams::Workflow, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+RSpec.describe Curator::Metastreams::Workflow, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
