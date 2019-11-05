ae666f534a26252668da64d3ba9e2ca277719ae8
bbarberBPL <bbarber@bpl.org>
2019-11-05 13:04:33 -0500

    Rubocop cleanup from previous commits

--- spec/models/curator/shared/controlled_terms/nomenclature.rb
+++ spec/models/curator/shared/controlled_terms/nomenclature.rb
@@ -4,19 +4,17 @@ require_relative '../optimistic_lockable.rb'
 require_relative '../timestampable.rb'
 
 RSpec.shared_examples 'nomenclature', type: :model do
-
   it { is_expected.to be_a_kind_of(Curator::ControlledTerms::Nomenclature) }
 
   it_behaves_like 'optimistic_lockable'
   it_behaves_like 'timestampable'
 
-  it { is_expected.to have_db_column(:type).of_type(:string).with_options(null: false ) }
-  it { is_expected.to have_db_column(:term_data).of_type(:jsonb).with_options(null: false ) }
+  it { is_expected.to have_db_column(:type).of_type(:string).with_options(null: false) }
+  it { is_expected.to have_db_column(:term_data).of_type(:jsonb).with_options(null: false) }
   it { is_expected.to have_db_column(:authority_id).of_type(:integer) }
 
   it { is_expected.to have_db_index(:authority_id) }
   it { is_expected.to have_db_index(:type) }
   it { is_expected.to have_db_index(:term_data) }
-  it { is_expected.to have_db_index("((term_data ->> 'id_from_auth'::text))") }
-
+  it { is_expected.to have_db_index("(((term_data ->> 'id_from_auth'::text))::character varying)") }
 end
--- spec/support/factory_helpers.rb
+++ spec/support/factory_helpers.rb
@@ -2,10 +2,10 @@
 
 module FactoryHelpers
   module FactoryFor
-    #converts the described class constant to a underscored symbol for using the factory bot methods.
-    #Useful for shared example classes
+    # converts the described class constant to a underscored symbol for using the factory bot methods.
+    # Useful for shared example classes
     def factory_key_for(desc_class)
-      desc_class.to_s.underscore.gsub('/', '_').to_sym
+      desc_class.to_s.underscore.tr!('/', '_').to_sym
     end
   end
 end
