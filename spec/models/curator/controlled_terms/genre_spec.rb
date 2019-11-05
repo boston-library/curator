527a6ea16e5348e3722e95cf8126cfcdb622aca5
bbarberBPL <bbarber@bpl.org>
2019-11-05 15:34:11 -0500

    Authority delegation shared tests done for nomenclature classes
    Authority Spec tests for cannonical name callback

--- spec/models/curator/controlled_terms/genre_spec.rb
+++ spec/models/curator/controlled_terms/genre_spec.rb
@@ -2,7 +2,9 @@
 
 require 'rails_helper'
 require_relative '../shared/controlled_terms/nomenclature'
+require_relative '../shared/controlled_terms/authority_delegation'
 
 RSpec.describe Curator::ControlledTerms::Genre, type: :model do
   it_behaves_like 'nomenclature'
+  it_behaves_like 'authority_delegation'
 end
