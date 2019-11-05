bb6fa056d3c9a64427e729542d8acc2adb5af3a4
Eben English <eenglish@bpl.org>
2019-11-05 12:10:35 -0500

    working specs for FileFactoryService

--- app/services/curator/filestreams/file_factory_service.rb
+++ app/services/curator/filestreams/file_factory_service.rb
@@ -15,10 +15,11 @@ module Curator
         ActiveStorage::Attachment.transaction do
           file_set = Curator::Filestreams::FileSet.find_by(ark_id: file_set_ark_id)
           raise "FileSet #{file_set_ark_id} not found!" unless file_set
-          attachment_type = @json_attrs.fetch('file_type', '').underscore
+          attachment_type = attachment_for_ds(@json_attrs.fetch('file_type', ''))
           filename = @json_attrs.fetch('file_name', nil)
           content_type = @json_attrs.fetch('content_type', nil)
           metadata = @json_attrs.fetch('metadata', {})
+
           # TODO: we'll probably need some better logic for this
           file_to_attach = File.open(metadata.fetch('ingest_filepath', nil))
           file_set.send(attachment_type).attach(io: file_to_attach,
@@ -36,6 +37,16 @@ module Curator
       rescue StandardError => e
         puts e.to_s
       end
+    end
+
+    private
+
+    # format legacy datastream names as ActiveStorage Attachement types
+    def attachment_for_ds(datastream_name)
+      return nil if datastream_name.blank?
+      attachment_type = datastream_name.underscore
+      attachment_type.insert(-4, '_') if datastream_name.match? /(300|800|marcxml)\z/
+      attachment_type
     end
   end
 end
