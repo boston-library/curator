e4207c7c7ac794685a289c892f40a301b3602d71
bbarberBPL <bbarber@bpl.org>
2019-11-13 10:04:52 -0500

    modified workflow factory to use file name with no extension for ingest origin

--- spec/factories/curator/metastreams/workflows.rb
+++ spec/factories/curator/metastreams/workflows.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     association :workflowable, factory: :curator_digital_object
     publishing_state { 1 }
     processing_state { 1 }
-    ingest_origin { Faker::File.file_name(ext: 'txt') }
+    ingest_origin { Faker::Internet.uuid }
     archived_at { nil }
   end
 end
