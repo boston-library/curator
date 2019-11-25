ea6cbcfcd9d90889c21291a46b3e8d96e623e4aa
bbarberBPL <bbarber@bpl.org>
2019-11-25 17:02:44 -0500

    Building out the serializer more...

--- lib/curator/serializers.rb
+++ lib/curator/serializers.rb
@@ -4,9 +4,8 @@ module Curator
   module Serializers
     extend ActiveSupport::Autoload
 
-    eager_autoload do
-      autoload :Resource
-    end
+    CACHE_KEY_PREFIX = 'curator-serializers'.freeze
+    VALID_FORMAT_KEYS=%i(json xml all).freeze
 
   end
 end
