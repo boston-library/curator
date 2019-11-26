0673fe13e36bfdab1a62d2862ed1dcad199e1116
bbarberBPL <bbarber@bpl.org>
2019-11-26 09:47:53 -0500

    Started building out adapter layer

--- lib/curator/serializers/adapter.rb
+++ lib/curator/serializers/adapter.rb
@@ -2,22 +2,24 @@
 
 module Curator
   module Serializers
+
     class Adapter
       def initialize(record_view_hash, serializer_options = {})
         @record_view_hash = record_hash
         @serializer_options = {}
       end
 
-      def serializable_hash(record_hash, options={})
+      def serializable_hash(record_view_hash, options={})
+        raise
       end
 
-      def as_json(options = {})
-        serializable_hash(options)
-      end
-
-      def to_json(options = {})
-        Oj.dump(serializable_hash(options))
-      end
+      # def as_json(options = {})
+      #   serializable_hash(options)
+      # end
+      #
+      # def to_json(options = {})
+      #   Oj.dump(serializable_hash(options))
+      # end
     end
   end
 end
