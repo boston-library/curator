ee7ddc8b96ed824c8428f945a5b1a6f5b58e0db2
bbarberBPL <bbarber@bpl.org>
2019-12-06 09:44:56 -0500

    Renamed a bunch of files/ refactoring schema classes

--- lib/curator/serializers/adapters/json_adapter.rb
+++ lib/curator/serializers/adapters/json_adapter.rb
@@ -6,13 +6,10 @@ module Curator
 
       def serializable_hash
       end
-      alias_method :as_json, :serializable_hash
 
       def render
         Oj.dump(serializable_hash(serializer_options))
       end
-      alias_method :to_json, :render
-
     end
   end
 end
--- lib/curator/serializers/serialized_attrs/relation_collection.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Curator
-  module Serializers
-    class RelationCollection < Relation
-    end
-  end
-end
