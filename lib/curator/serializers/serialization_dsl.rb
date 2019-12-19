9e3b31a02cfba4b46d7c41dbd22df107b8de06dc
bbarberBPL <bbarber@bpl.org>
2019-12-19 17:04:28 -0500

    Started Prototyping the DSL

--- lib/curator/serializers/serialization_dsl.rb
+++ lib/curator/serializers/serialization_dsl.rb
@@ -3,6 +3,31 @@
 module Curator
   module Serializers
     module SerializationDSL
+      extend ActiveSupport::Concern
+      class << self
+        attr_reader :_schema_adapters
+      end
+
+
+      class_methods do
+        def define_schema(adapter:, root: nil, options = {}, &block)
+          raise 'NullAdapter cant be used this way' if adapter.to_sym == :null
+
+          schema_adapter_klass = Curator::Serializers.lookup_adapter(adapter)
+          #TODO Think of mor options to set up at the schema level.
+          schema_options = options.dup.slice(:cached, :cached_length, :race_condition_ttyl, :key_transform_method)
+          schema_options.merge!(root: root) if root
+
+          map_schema_adapter(adapter, schema_adapter_klass.new(schema_options, &block))
+        end
+
+
+        private
+        def map_schema_adapter(adapter_key, schema_adapter)
+          @_schema_adapters = Concurrent::Map.new if @_schema_adapters.nil?
+          @_schema_adapter.merge_pair(adapter_key, schema_adapter)
+        end
+      end
       #Taken from https://github.com/wmakley/tiny_serializer/blob/master/lib/tiny_serializer/dsl.rb but using thread safe collections
 
       # private
