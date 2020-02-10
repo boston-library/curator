3a0c611e858eec99b205511b38cf599d9d35085e
bbarberBPL <bbarber@bpl.org>
2020-02-10 14:21:40 -0500

    Added Decorator Specs

---
+++ spec/decorators/curator/shared/curator_decorator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'curator_decorator', type: :decorators do
+  describe 'Curator::Decorators::BaseDecorator' do
+    subject { decorator }
+
+    it { is_expected.to be_a_kind_of(Curator::Decorators::BaseDecorator) }
+    it { is_expected.to respond_to(:blank?, :attributes, :serializable_hash) }
+
+    it 'expects #blank? to return true or false' do
+      expect(subject.blank?).to be(true).or be(false)
+    end
+
+    it 'expects #attributes to be a kind of Hash' do
+      expect(subject.attributes).to be_a_kind_of(Hash)
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -127,10 +127,13 @@ RSpec.configure do |config|
     metadata[:type] = :lib_serializers
   end
 
-  config.define_derived_metadata(file_path: Regexp.new('/spec/serializers/curator')) do |metadata|
+  config.define_derived_metadata(file_path: Regexp.new('/spec/serializers/')) do |metadata|
     metadata[:type] = :serializers
   end
 
+  config.define_derived_metadata(file_path: Regexp.new('/spec/decorators/')) do |metadata|
+    metadata[:type] = :decorators
+  end
   # TODO: Create derived metadata for serializers once we switch to blueprinter
 
   config.infer_spec_type_from_file_location!
