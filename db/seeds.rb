94e2270f3e5369d75dd327ca053595b0938b402a
Eben English <eben.english@gmail.com>
2020-03-24 17:02:50 -0400

    use BPLDC Authority API for seeding database [skip ci]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,78 +1,55 @@
 # frozen_string_literal: true
 
-AUTHORITY_INPUTS = [
-  { code: 'gmgpc', base_url: 'http://id.loc.gov/vocabulary/graphicMaterials' },
-  { code: 'lctgm', base_url: 'http://id.loc.gov/vocabulary/graphicMaterials' },
-  { code: 'naf', base_url: 'http://id.loc.gov/authorities/names' },
-  { code: 'lcsh', base_url: 'http://id.loc.gov/authorities/subjects' },
-  { code: 'lcgft', base_url: 'http://id.loc.gov/authorities/genreForms' },
-  { code: 'iso639-2', base_url: 'http://id.loc.gov/vocabulary/iso639-2' },
-  { code: 'marcrelator', base_url: 'http://id.loc.gov/vocabulary/relators' },
-  { code: 'aat', base_url: 'http://vocab.getty.edu/aat' },
-  { code: 'tgn', base_url: 'http://vocab.getty.edu/tgn', name: 'Thesaurus of Geographic Names' },
-  { code: 'ulan', base_url: 'http://vocab.getty.edu/ulan', name: 'Getty Union List of Artist Names' },
-  { code: 'geonames', base_url: 'http://sws.geonames.org/', name: 'GeoNames' },
-  { code: 'resourceTypes', base_url: 'http://id.loc.gov/vocabulary/resourceTypes' },
-  { code: 'marcgt', base_url: 'http://id.loc.gov/vocabulary/genreFormSchemes/marcgt', name: 'MARC genre terms' },
-  { code: 'rbgenr', base_url: 'https://rbms.info/vocabularies/genre', name: 'RBMS Controlled Vocabularies: Genre Terms' },
-  { code: 'rbpri', base_url: 'https://rbms.info/vocabularies/printing-publishing', name: 'RBMS Controlled Vocabularies: Printing & Publishing Evidence' },
-  { code: 'rbprov', base_url: 'https://rbms.info/vocabularies/provenance', name: 'RBMS Controlled Vocabularies: Provenance Evidence' },
-  { code: 'local', name: 'local' }
-].freeze
+# fetch data from source authority and seed database for input validation
 
-AUTHORITY_INPUTS.each do |auth_input|
+# ControlledTerms::Authority
+authorities_url = "#{ENV['AUTHORITY_API_URL']}/bpldc/authorities"
+auth_data = Curator::ControlledTerms::CannonicalLabelService.call(url: authorities_url, json_path: nil)
+auth_data&.each do |auth_input|
   Curator.controlled_terms.authority_class.transaction do
     begin
-      puts "Seeding authority with attributes #{auth_input.inspect}"
+      puts "Seeding ControlledTerms::Authority with id: #{auth_input[:code]}"
       Curator.controlled_terms.authority_class.where(auth_input).first_or_create!
     rescue StandardError => e
-      puts "Failed to seed Authority Record with the following input #{auth_input.inspect}"
+      puts "Failed to seed ControlledTerms::Authority with input #{auth_input.inspect}"
       puts e.inspect
     end
   end
 end
 
-BASIC_GENRE_INPUTS = {
-  'gmgpc' => %w(tgm001686 tgm003185 tgm003279 tgm003634 tgm012286 tgm006261 tgm007393 tgm007721 tgm008104 tgm008237 tgm007641 tgm001221 tgm000229),
-  'lctgm' => %w(tgm002590 tgm007159 tgm007068 tgm009874 tgm006804 tgm006926 tgm006906)
-}.freeze
-
-BASIC_GENRE_INPUTS.each do |auth_code, ids_from_auth|
-  Curator.controlled_terms.genre_class.transaction do
-    begin
-      puts "seeding with authority code #{auth_code}..."
-      auth = Curator.controlled_terms.authority_class.find_by(code: auth_code)
-      ids_from_auth.each do |id_from_auth|
-        puts "Seeding Basic Genre #{id_from_auth}..."
-        auth.genres.where(term_data: { basic: true, id_from_auth: id_from_auth }).first_or_create!
+# ControlledTerms::Genre, ControlledTerms::ResourceType, ControlledTerms::Language, ControlledTerms::Role
+terms_for_seed = %w(basic_genres resource_types languages roles)
+terms_for_seed.each do |term_for_seed|
+  terms_url = "#{ENV['AUTHORITY_API_URL']}/bpldc/#{term_for_seed}"
+  terms_data = Curator::ControlledTerms::CannonicalLabelService.call(url: terms_url, json_path: nil)
+  terms_data&.each do |term_data|
+    term_class_name = term_for_seed.gsub(/\Abasic_/, '').singularize
+    term_class = Curator.controlled_terms.public_send("#{term_class_name}_class")
+    term_class.transaction do
+      begin
+        auth = Curator.controlled_terms.authority_class.find_by(code: term_data[:authority_code])
+        puts "Seeding #{term_class} with id: #{term_data[:id_from_auth]}..."
+        term_data[:basic] = true if term_class == Curator::ControlledTerms::Genre
+        term_class.where(term_data: term_data.except(:authority_code), authority: auth).first_or_create!
+      rescue StandardError => e
+        puts "====Failed to seed #{term_class}: '#{term_data[:label]}' (#{term_data[:id_from_auth]}) for authority:
+#{term_data[:authority_code]}===="
+        puts e.inspect
       end
-    rescue StandardError => e
-      puts "====Failed to seed Basic Genre for the following authority #{auth_code}===="
-      puts e.inspect
     end
   end
 end
 
-LICENSE_INPUTS = [
-  { label: 'No known restrictions on use.', uri: nil },
-  { label: 'This work is in the public domain under a Creative Commons No Rights Reserved License (CC0).', uri: nil },
-  { label: 'This work is licensed for use under a Creative Commons Attribution License (CC BY).', uri: 'https://creativecommons.org/licenses/by/4.0' },
-  { label: 'This work is licensed for use under a Creative Commons Attribution Share Alike License (CC BY-SA).', uri: 'https://creativecommons.org/licenses/by-sa/4.0' },
-  { label: 'This work is licensed for use under a Creative Commons Attribution No Derivatives License (CC BY-ND).', uri: 'https://creativecommons.org/licenses/by-nd/4.0' },
-  { label: 'This work is licensed for use under a Creative Commons Attribution Non-Commercial License (CC BY-NC).', uri: 'https://creativecommons.org/licenses/by-nc/4.0' },
-  { label: 'This work is licensed for use under a Creative Commons Attribution Non-Commercial Share Alike License (CC BY-NC-SA).', uri: 'https://creativecommons.org/licenses/by-nc-sa/4.0' },
-  { label: 'This work is licensed for use under a Creative Commons Attribution Non-Commercial No Derivatives License (CC BY-NC-ND).', uri: 'https://creativecommons.org/licenses/by-nc-nd/4.0' },
-  { label: 'All rights reserved.', uri: nil },
-  { label: 'Contact host institution for more information.', uri: nil }
-].freeze
-
-LICENSE_INPUTS.each do |license_input|
-  puts "Seeding License with attributes #{license_input}"
+# ControlledTerms::License
+licenses_url = "#{ENV['AUTHORITY_API_URL']}/bpldc/licenses"
+licenses_data = Curator::ControlledTerms::CannonicalLabelService.call(url: licenses_url, json_path: nil)
+licenses_data&.each do |license_input|
   Curator.controlled_terms.license_class.transaction do
     begin
+      puts "Seeding License with attributes: #{license_input}"
       Curator.controlled_terms.license_class.where(term_data: license_input).first_or_create!
     rescue StandardError => e
-      puts "Failed to seed License for the following inpput #{license_input.inspect}"
+      puts "Failed to seed License with attributes: #{license_input}"
       puts e.inspect
     end
   end
