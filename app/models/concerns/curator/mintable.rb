588f47f88e4977b1f0d46ccddb08c6a27e05d3a9
bbarberBPL <bbarber@bpl.org>
2020-04-01 18:22:22 -0400

    Intermitten spec failures with file set factory service appear to have stopped [skip ci]

--- app/models/concerns/curator/mintable.rb
+++ app/models/concerns/curator/mintable.rb
@@ -4,20 +4,16 @@ module Curator
   module Mintable
     extend ActiveSupport::Concern
     included do
-      before_validation :generate_ark_id, on: :create, unless: :has_ark_id?
+      before_validation :generate_ark_id, on: :create
 
       validates :ark_id, presence: true, uniqueness: true, on: :create
     end
     # Todo put a before validate callback here to the ark manager
 
-    def has_ark_id?
-      ark_id.present?
-    end
-
     private
 
     def generate_ark_id
-      self.ark_id = Curator::MinterService.call
+      self.ark_id = Curator::MinterService.call unless ark_id.present?
     end
   end
 end
--- app/services/curator/filestreams/file_set_factory_service.rb
+++ app/services/curator/filestreams/file_set_factory_service.rb
@@ -9,46 +9,47 @@ module Curator
         object_ark_id = @json_attrs.dig('file_set_of', 'ark_id')
         file_set_type = @json_attrs.fetch('file_set_type', {})
         obj = Curator.digital_object_class.find_by(ark_id: object_ark_id)
-        @record = Curator.filestreams.send("#{file_set_type}_class").where(ark_id: @ark_id).first_or_create! do |file_set|
-          file_set.file_set_of = obj
-          file_set.file_name_base = @json_attrs.fetch('file_name_base')
-          file_set.position = @json_attrs.fetch('position', 0)
-          file_set.pagination = @json_attrs.fetch('pagination', {})
-          file_set.created_at = @created if @created
-          file_set.updated_at = @updated if @updated
 
-          # set exemplary relationships
-          if Curator::Mappings::ExemplaryImage::VALID_EXEMPLARY_FILE_SET_TYPES.include?(file_set_type.classify)
-            exemplary_ids = @json_attrs.fetch('exemplary_image_of', []).pluck('ark_id')
-            exemplary_ids.each do |ex_ark_id|
-              ex_obj = Curator.digital_object_class.find_by(ark_id: ex_ark_id) ||
-                       Curator.collection_class.find_by(ark_id: ex_ark_id)
+        @record = Curator.filestreams.send("#{file_set_type}_class").find_or_initialize_by(ark_id: @ark_id)
+        @record.file_set_of = obj
+        @record.file_name_base = @json_attrs.fetch('file_name_base')
+        @record.position = @json_attrs.fetch('position', 0)
+        @record.pagination = @json_attrs.fetch('pagination', {})
+        @record.created_at = @created if @created
+        @record.updated_at = @updated if @updated
 
-              raise ActiveRecord::RecordNotSaved, "Bad exemplary id! #{ex_ark_id} is either not in the repo or is not a DigitalObject or Collection" unless ex_obj
+        # set exemplary relationships
+        if Curator::Mappings::ExemplaryImage::VALID_EXEMPLARY_FILE_SET_TYPES.include?(file_set_type.classify)
+          exemplary_ids = @json_attrs.fetch('exemplary_image_of', []).pluck('ark_id')
+          exemplary_ids.each do |ex_ark_id|
+            ex_obj = Curator.digital_object_class.find_by(ark_id: ex_ark_id) ||
+                     Curator.collection_class.find_by(ark_id: ex_ark_id)
 
-              ex_obj.lock!
-              build_exemplary(file_set) do |exemplary_img|
-                exemplary_img.exemplary_object = ex_obj
-              end
+            raise ActiveRecord::RecordNotSaved, "Bad exemplary id! #{ex_ark_id} is either not in the repo or is not a DigitalObject or Collection" unless ex_obj
+
+            ex_obj.lock!
+            build_exemplary(@record) do |exemplary_img|
+              exemplary_img.exemplary_object = ex_obj
             end
           end
-
-          build_workflow(file_set) do |workflow|
-            workflow.send('ingest_origin=', @workflow_json_attrs.fetch(:ingest_origin, ENV['HOME'].to_s))
-            processing_state = @workflow_json_attrs.fetch(:processing_state, nil)
-            publishing_state = obj&.workflow&.publishing_state
-            # set publishing state to same value as parent DigitalObject
-            workflow.send('processing_state=', processing_state) if processing_state
-            workflow.send('publishing_state=', publishing_state) if publishing_state
-          end
-
-          # TODO: set access_edit_group permissions
-          build_administrative(file_set) do |administrative|
-            access_edit_group = nil
-            # access_edit_group = @admin_json_attrs.fetch(:access_edit_group, nil)
-            administrative.send('access_edit_group=', access_edit_group) if access_edit_group
-          end
         end
+
+        build_workflow(@record) do |workflow|
+          workflow.send('ingest_origin=', @workflow_json_attrs.fetch(:ingest_origin, ENV['HOME'].to_s))
+          processing_state = @workflow_json_attrs.fetch(:processing_state, nil)
+          publishing_state = obj&.workflow&.publishing_state
+          # set publishing state to same value as parent DigitalObject
+          workflow.send('processing_state=', processing_state) if processing_state
+          workflow.send('publishing_state=', publishing_state) if publishing_state
+        end
+
+        # TODO: set access_edit_group permissions
+        build_administrative(@record) do |administrative|
+          access_edit_group = nil
+          # access_edit_group = @admin_json_attrs.fetch(:access_edit_group, nil)
+          administrative.send('access_edit_group=', access_edit_group) if access_edit_group
+        end
+        @record.save!
       end
       return @success, @result
     end
