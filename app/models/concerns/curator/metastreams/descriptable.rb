b552f1d6efd3b3f400b6ab532f409b37d41f4be7
Eben English <eben.english@gmail.com>
2020-04-29 12:55:41 -0400

    use autosave to save and reindex Metastreamable relationships

--- app/models/concerns/curator/metastreams/descriptable.rb
+++ app/models/concerns/curator/metastreams/descriptable.rb
@@ -6,7 +6,9 @@ module Curator
       extend ActiveSupport::Concern
       included do
         scope :with_descriptive, -> { joins(:descriptive).includes(:descriptive) }
-        has_one :descriptive, -> { merge(with_physical_location).merge(with_license).merge(with_desc_terms) }, as: :descriptable, inverse_of: :descriptable, class_name: 'Curator::Metastreams::Descriptive', dependent: :destroy
+        has_one :descriptive, -> { merge(with_physical_location).merge(with_license).merge(with_desc_terms) },
+                as: :descriptable, inverse_of: :descriptable, class_name: 'Curator::Metastreams::Descriptive',
+                dependent: :destroy, autosave: true
 
         validates :descriptive, presence: true
         validates_associated :descriptive, on: :create
