3f5baabd233a7529ddf39dc3d735439ac57872d5
bbarberBPL <bbarber@bpl.org>
2020-05-11 13:24:12 -0400

    Setting up descritpives controller

--- app/controllers/curator/metastreams/descriptives_controller.rb
+++ app/controllers/curator/metastreams/descriptives_controller.rb
@@ -25,7 +25,26 @@ module Curator
     def descriptive_params
       case params[:action]
       when 'update'
-        params.require(:descriptive).permit!
+        params.require(:descriptive).permit(:abstract,
+                                            :access_restrictions,
+                                            :digital_origin,
+                                            :extent,
+                                            :frequency,
+                                            :issuance,
+                                            :origin_event,
+                                            :physical_location_department,
+                                            :physical_location_shelf_locator,
+                                            :place_of_publication,
+                                            :publisher,
+                                            :resource_type_manuscript,
+                                            :rights,
+                                            :series,
+                                            :subseries,
+                                            :subsubseries,
+                                            :text_direction,
+                                            :toc,
+                                            :toc_url
+                                          )
       else
         params
       end
