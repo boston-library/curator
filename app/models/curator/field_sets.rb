0f8fe1c7398c2ef5895fc9cb295b292f08d7e92b
bbarberBPL <bbarber@bpl.org>
2020-06-24 16:55:05 -0400

    Removed old/uncessary files for descriptive namespace and field_sets [skip ci]

--- app/models/curator/field_sets.rb
+++ app/models/curator/field_sets.rb
@@ -3,10 +3,11 @@
 module Curator
   module FieldSets
     extend Curator::NamespaceAccessor
-    
+
     IDENTIFIER_TYPES = ['local-accession', 'local-other', 'local-call', 'local-barcode', 'internet-archive', 'isbn', 'ismn', 'isrc', 'issn', 'issue-number', 'lccn', 'matrix-number', 'music-plate', 'music-publisher', 'sici', 'uri', 'videorecording'].freeze
+
     NOTE_TYPES = ['date', 'language', 'acquisition', 'ownership', 'funding', 'biographical/historical', 'citation/reference', 'preferred citation', 'bibliography', 'exhibitions', 'publications', 'creation/production credits', 'performers', 'physical description', 'venue', 'arrangement', 'statement of responsibility'].freeze
-    
+
     namespace_klass_accessors :cartographic, :date, :identifier, :note, :publication, :related, :subject, :title_set, :title
   end
 end
