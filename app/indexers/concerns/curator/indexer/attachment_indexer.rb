ea1bebf0fa319e3a7620f95e26d3e29c909a99a9
Eben English <eben.english@gmail.com>
2020-12-16 16:35:43 -0500

    fixes for #113 review

--- app/indexers/concerns/curator/indexer/attachment_indexer.rb
+++ app/indexers/concerns/curator/indexer/attachment_indexer.rb
@@ -5,31 +5,32 @@ module Curator
   class Indexer < Traject::Indexer
     module AttachmentIndexer
       extend ActiveSupport::Concern
+
+      ATTACHMENT_FIELDS = %i(byte_size content_type checksum).freeze
+      HAS_ONE_ATTACHMENT_TYPES = %i(audio_access audio_master document_access document_master
+                                    ebook_access_epub ebook_access_mobi ebook_access_daisy
+                                    image_access_800 image_georectified_master image_master image_negative_master
+                                    image_service image_thumbnail_300 metadata_foxml metadata_ia metadata_ia_scan
+                                    metadata_marc_xml metadata_mods metadata_oai text_coordinates_access
+                                    text_coordinates_master text_plain video_access video_master).freeze
+      # HAS_MANY_ATTACHMENT_TYPES = %i().freeze
+
       included do
         configure do
           each_record do |record, context|
             attachments = {}
-            attachment_fields = %i(byte_size content_type checksum).freeze
-
-            has_one_attachment_types = %i(audio_access audio_master document_access document_master
-                                          ebook_access_epub ebook_access_mobi ebook_access_daisy
-                                          image_access_800 image_georectified_master image_master image_negative_master
-                                          image_service image_thumbnail_300 metadata_foxml metadata_ia metadata_ia_scan
-                                          metadata_marc_xml metadata_mods metadata_oai text_coordinates_access
-                                          text_coordinates_master text_plain video_access video_master).freeze
-            has_one_attachment_types.each do |attachment_type|
-              next unless record.respond_to?(attachment_type) && record.send(attachment_type).attached?
+            HAS_ONE_ATTACHMENT_TYPES.each do |attachment_type|
+              next unless record.respond_to?(attachment_type) && record.public_send(attachment_type).attached?
 
               attachment_hash = {}
-              attachment_hash[:filename] = record.send(attachment_type).filename.to_s
-              attachment_fields.each do |blob_attr|
-                attachment_hash[blob_attr] = record.send(attachment_type).public_send(blob_attr)
+              attachment_hash[:filename] = record.public_send(attachment_type).filename.to_s
+              ATTACHMENT_FIELDS.each do |blob_attr|
+                attachment_hash[blob_attr] = record.public_send(attachment_type).public_send(blob_attr)
               end
               attachments[attachment_type] = attachment_hash
             end
 
-            # has_many_attachment_types = []
-            # has_many_attachment_types.each do |attachment_type|
+            # HAS_MANY_ATTACHMENT_TYPES.each do |attachment_type|
             #   next unless record.respond_to?(attachment_type) && record.send(attachment_type).attached?
 
             #   blobs = record.public_send(attachment_type).blobs
@@ -38,7 +39,7 @@ module Curator
             #   attachments_arr = blobs.inject([]) do |ba, blob|
             #     attachment_hash = {}
             #     attachment_hash[:filename] = blob.filename.to_s
-            #     attachment_fields.each do |blob_attr|
+            #     ATTACHMENT_FIELDS.each do |blob_attr|
             #       attachment_hash[blob_attr] = blob.public_send(blob_attr)
             #     end
             #     ba << attachment_hash
