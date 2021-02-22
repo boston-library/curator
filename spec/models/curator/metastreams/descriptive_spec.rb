abe118d38ae004fff09b741e53b8f4c49b71a0e9
Eben English <eben.english@gmail.com>
2021-02-22 16:51:08 -0500

    more refactoring of restore version spec

--- spec/models/curator/metastreams/descriptive_spec.rb
+++ spec/models/curator/metastreams/descriptive_spec.rb
@@ -354,10 +354,10 @@ RSpec.describe Curator::Metastreams::Descriptive, type: :model do
     describe 'restoring previous version' do
       it 'resets the object to previous state' do
         version_to_restore = desc_obj.versions[1]
-        restored_obj = version_to_restore.reify(has_many: true, has_one: true, belongs_to: true, mark_for_destruction: true)
-        restored_obj.save!
-        expect(restored_obj.publisher).to_not eq new_publisher
-        expect(restored_obj.genres.count).to eq 1
+        desc_obj = version_to_restore.reify(has_many: true, has_one: true, belongs_to: true, mark_for_destruction: true)
+        desc_obj.save!
+        expect(desc_obj.publisher).to_not eq new_publisher
+        expect(desc_obj.genres.count).to eq 1
       end
     end
   end
