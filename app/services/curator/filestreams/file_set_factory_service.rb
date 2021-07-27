boston-library/curator

commit 9e74fb429989dafc0702726bf08f030ea5f50ed0

Resolve 160 165 (#167)
* Refactored adding/removing relationships in factory services

* Added ark remove service/background job for soft deleting arks in database

* Refactored specs

* Added Faraday to webmock to allow more robust VCR cassettes and recorded new episodes for some existing ones

* Updated addressable due to security vulnerability

* Resolves #165, Fixes #162

app/services/curator/filestreams/file_set_factory_service.rb
@@ -63,12 +63,13 @@ module Curator
       return if exemplary_ids.blank?
 
       exemplary_ids.each do |ex_ark_id|
-        ex_obj = Curator.digital_object_class.find_by(ark_id: ex_ark_id) ||
-                 Curator.collection_class.find_by(ark_id: ex_ark_id)
+        ex_obj = Curator.digital_object_class.select(:id, :ark_id).find_by(ark_id: ex_ark_id) ||
+                 Curator.collection_class.select(:id, :ark_id).find_by(ark_id: ex_ark_id)
 
         raise ActiveRecord::RecordNotSaved, "Bad exemplary id! #{ex_ark_id} is either not in the repo or is not a DigitalObject or Collection" unless ex_obj
 
-        ex_obj.lock!
+        next if !file_set.new_record? && file_set.exemplary_image_of_mappings.exist?(exemplary_object: ex_object)
+
         build_exemplary(file_set) do |exemplary_img|
           exemplary_img.exemplary_object = ex_obj
         end

.travis.yml
@@ -19,7 +19,7 @@ before_install:
   - sudo cp /etc/postgresql/{9.3,12}/main/pg_hba.conf
   - sudo pg_ctlcluster 12 main restart
   - gem update --system --no-document
-  - gem install bundler:2.2.21 --no-document
+  - gem install bundler:2.2.23 --no-document
 
 before_script:
   - bundle exec rails app:curator:setup

app/indexers/concerns/curator/indexable.rb
@@ -54,7 +54,6 @@ module Curator
       class_attribute :curator_indexable_auto_callbacks, default: true
 
       after_save_commit :queue_indexing_job, if: -> { Curator::Indexable.auto_callbacks?(self) }
-      after_destroy :indexer_health_check
       after_destroy_commit :queue_deletion_job, if: -> { Curator::Indexable.auto_callbacks?(self) }
     end
 
@@ -64,12 +63,6 @@ module Curator
     #  - a per-update writer, or thread/block-specific writer configured with `self.index_with`
     def update_index(mapper: curator_indexable_mapper, writer: nil)
       RecordIndexUpdater.new(self, mapper: mapper, writer: writer).update_index
-    end
-
-    # make sure indexing and authority services are ready before we commit transactions and :update_index
-    def indexer_health_check
-      raise Curator::Exceptions::SolrUnavailable unless SolrUtil.ready?
-      raise Curator::Exceptions::AuthorityApiUnavailable unless Curator::ControlledTerms::AuthorityService.ready?
     end
 
     def queue_indexing_job

app/indexers/concerns/curator/indexer/geographic_indexer.rb
@@ -26,7 +26,7 @@ module Curator
               context.output_hash['subject_geo_label_sim'] << geo_label if geo_auth
 
               if geo_auth == 'tgn' || geo_auth == 'geonames'
-                auth_url = "/#{geo_auth}/#{subject_geo.id_from_auth}"
+                auth_url = "#{geo_auth}/#{subject_geo.id_from_auth}"
                 auth_data = Curator::ControlledTerms::AuthorityService.call(path: auth_url, path_prefix: '/geomash')
                 if auth_data && auth_data[:hier_geo].present?
                   if auth_data[:non_hier_geo].present?

app/jobs/curator/ark_delete_job.rb
@@ -1,0 +1,19 @@
+# frozen_string_literal: true
+
+module Curator
+  class ArkDeleteJob < ApplicationJob
+    queue_as :arks
+
+    retry_on Curator::Exceptions::ArkManagerApiUnavailable, ActiveRecord::RecordNotDestroyed, attempts: 3
+
+    before_perform do
+      minter_service_available = Curator::MinterService.ready?
+
+      raise Curator::Exceptions::ArkManagerApiUnavailable if !minter_service_available
+    end
+
+    def perform(ark_id)
+      Curator::ArkDeleteService.call(ark_id)
+    end
+  end
+end

app/jobs/curator/indexer/deletion_job.rb
@@ -4,6 +4,13 @@ module Curator
   class Indexer::DeletionJob < ApplicationJob
     queue_as :indexing
 
+    retry_on Curator::Exceptions::SolrUnavailable, Curator::Exceptions::AuthorityApiUnavailable, ActiveRecord::StaleObjectError, attempts: 3
+
+    before_perform do
+      raise Curator::Exceptions::SolrUnavailable if !Curator::SolrUtil.ready?
+      raise Curator::Exceptions::AuthorityApiUnavailable if !Curator::ControlledTerms::AuthorityService.ready?
+    end
+
     def perform(ark_id)
       writer = Curator.config.indexable_settings.writer_instance!
       writer.delete(ark_id)

app/jobs/curator/indexer/indexing_job.rb
@@ -4,10 +4,14 @@ module Curator
   class Indexer::IndexingJob < ApplicationJob
     queue_as :indexing
 
-    retry_on Curator::Exceptions::SolrUnavailable, Curator::Exceptions::AuthorityApiUnavailable, attempts: 3
+    retry_on Curator::Exceptions::SolrUnavailable, Curator::Exceptions::AuthorityApiUnavailable, ActiveRecord::StaleObjectError, attempts: 3
+
+    before_perform do
+      raise Curator::Exceptions::SolrUnavailable if !Curator::SolrUtil.ready?
+      raise Curator::Exceptions::AuthorityApiUnavailable if !Curator::ControlledTerms::AuthorityService.ready?
+    end
 
     def perform(obj_to_index)
-      obj_to_index.indexer_health_check
       obj_to_index.update_index
     end
   end

app/models/concerns/curator/mintable.rb
@@ -10,6 +10,8 @@ module Curator
       before_validation :generate_ark_id, on: :create
 
       validates :ark_id, presence: true, uniqueness: true, on: :create
+
+      after_destroy_commit :destroy_ark
     end
 
     module InstanceMethods
@@ -19,6 +21,10 @@ module Curator
       # rubocop:disable Metrics/LineLength
       # TODO: We should validate the arks have actual entries in the ark manager database on on create. Otherwise the permalink urls will be broken. As well as the the other functionalities the ark manager provides. I'm currently holding off until we are done testing but ideally we should implment this check once we are ready for the migration on prod. Also we should add an after_destroy commit that "deletes" the ark entries. This won't actually delet them since they will just be flagged as actuve = false. Ideally we should put this in a background job.
       # rubocop:enable Metrics/LineLength
+
+      def destroy_ark
+        Curator::ArkDeleteJob.perform_later(ark_id)
+      end
 
       def generate_ark_id
         return if ark_id.present?

commit 4ee3d983f083157461e85b54c6d901e50373d29f

fix regression introduced by 9e74fb429989dafc0702726bf08f030ea5f50ed0

app/services/curator/filestreams/file_set_factory_service.rb
@@ -68,7 +68,7 @@ module Curator
 
         raise ActiveRecord::RecordNotSaved, "Bad exemplary id! #{ex_ark_id} is either not in the repo or is not a DigitalObject or Collection" unless ex_obj
 
-        next if !file_set.new_record? && file_set.exemplary_image_of_mappings.exist?(exemplary_object: ex_object)
+        next if !file_set.new_record? && file_set.exemplary_image_of_mappings.exist?(exemplary_object: ex_obj)
 
         build_exemplary(file_set) do |exemplary_img|
           exemplary_img.exemplary_object = ex_obj