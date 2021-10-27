06465234eb3c426fb8d45c3fa06212479421e531
Eben English <ebenenglish@users.noreply.github.com>
2021-10-27 16:14:27 -0400

    add missing issn field, clean up a bit (#187)
    * add missing issn field, clean up a bit
    
    * make id fields a constant; add fields for all id types

--- app/indexers/concerns/curator/indexer/identifier_indexer.rb
+++ app/indexers/concerns/curator/indexer/identifier_indexer.rb
@@ -4,25 +4,25 @@ module Curator
   class Indexer < Traject::Indexer
     module IdentifierIndexer
       extend ActiveSupport::Concern
+
+      ID_FIELDS = %w(local_other_tsim local_other_invalid_tsim local_call_tsim local_call_invalid_tsim
+                     local_barcode_tsim local_barcode_invalid_tsim local_accession_tsim isbn_tsim
+                     lccn_tsim ia_id_ssi uri_ss iiif_manifest_ss uri_preview_ss issn_ssim ismn_ssim
+                     isrc_ssim issue_number_ssim matrix_number_ssim music_plate_ssim music_publisher_ssim
+                     sici_ssim videorecording_ssim).freeze
+      ID_URI_FIELD = 'identifier_uri_ss'
       included do
         configure do
           each_record do |record, context|
             next unless record.descriptive&.identifier
 
-            id_uri_field = 'identifier_uri_ss'
-
             if record.administrative.hosting_status == 'hosted' &&
                record.descriptive.identifier.find { |i| i.type == 'uri' }.blank?
               id_uri_value = "#{Curator.config.ark_manager_api_url}/ark:/#{Curator.config.default_ark_params[:namespace_ark]}/#{record.ark_id.split(':').last}"
-              context.output_hash[id_uri_field] = [id_uri_value]
+              context.output_hash[ID_URI_FIELD] = [id_uri_value]
             end
 
-            id_fields = %w(identifier_local_other_tsim identifier_local_other_invalid_tsim
-                           identifier_local_call_tsim identifier_local_call_invalid_tsim
-                           identifier_local_barcode_tsim identifier_local_barcode_invalid_tsim
-                           identifier_local_accession_tsim identifier_isbn_tsim identifier_lccn_tsim
-                           identifier_ia_id_ssi identifier_uri_ss identifier_iiif_manifest_ss identifier_uri_preview_ss)
-            id_fields.each { |field| context.output_hash[field] ||= [] }
+            ID_FIELDS.each { |field| context.output_hash["identifier_#{field}"] ||= [] }
             record.descriptive.identifier.each do |identifier|
               label = identifier.label
               id_type = identifier.type.underscore
@@ -33,11 +33,13 @@ module Curator
                            when 'internet_archive'
                              'identifier_ia_id_ssi'
                            when 'uri'
-                             id_uri_field
+                             ID_URI_FIELD
                            when 'uri_preview'
                              'identifier_uri_preview_ss'
                            when 'iiif_manifest'
                              'identifier_iiif_manifest_ss'
+                           when 'issn', 'ismn', 'isrc', 'issue_number', 'matrix_number', 'music_plate', 'music_publisher', 'sici', 'videorecording'
+                             "identifier_#{id_type}_ssim"
                            else
                              "identifier_#{id_type}_tsim"
                            end
