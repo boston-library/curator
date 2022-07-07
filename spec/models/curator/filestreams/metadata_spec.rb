a7e5433c654fd0d9a2cebc2b11cbecfb0feb7211
Eben English <eben.english@gmail.com>
2022-07-07 14:04:09 -0400

    update specs for Filestreams::Metadata

--- spec/models/curator/filestreams/metadata_spec.rb
+++ spec/models/curator/filestreams/metadata_spec.rb
@@ -24,4 +24,26 @@ RSpec.describe Curator::Filestreams::Metadata, type: :model do
       let(:has_one_file_attachments) { %i(metadata_ia metadata_ia_scan metadata_marc_xml metadata_mods metadata_oai) }
     end
   end
+
+  describe '#required_derivatives_complete?' do
+    it 'expects :DEFAULT_REQUIRED_DERIVATIVES to be a defined constant' do
+      expect(described_class).to be_const_defined(:DEFAULT_REQUIRED_DERIVATIVES)
+    end
+
+    it 'is expected to respond_to #required_derivatives_complete?' do
+      expect(subject).to respond_to(:required_derivatives_complete?).with(1).argument
+      expect(subject).to_not be_required_derivatives_complete
+    end
+  end
+
+  describe 'Callbacks' do
+    describe '.after_update_commit' do
+      subject { create(:curator_filestreams_metadata) }
+
+      it 'runs #set_as_exemplary callback on :update' do
+        expect(subject).to receive(:set_as_exemplary).at_least(:once)
+        subject.touch
+      end
+    end
+  end
 end
