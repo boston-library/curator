58be420dbc50d515b275ac5fa5ba9390e462b535
Eben English <eben.english@gmail.com>
2022-07-26 09:47:02 -0400

    add string field for institution and collection names (fixes #279)

--- spec/indexers/curator/institution_indexer_spec.rb
+++ spec/indexers/curator/institution_indexer_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe Curator::InstitutionIndexer do
     let(:indexed) { indexer.map_record(institution) }
 
     it 'sets the title fields' do
-      expect(indexed['title_info_primary_tsi']).to eq [institution.name]
+      %w(title_info_primary_tsi title_info_primary_ssi).each do |field|
+        expect(indexed[field]).to eq [institution.name]
+      end
       expect(
         indexed['title_info_primary_ssort']
       ).to eq [Curator::Parsers::InputParser.get_proper_title(institution.name).last]
