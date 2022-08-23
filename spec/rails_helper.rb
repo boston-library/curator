b1acae2fa8a8e11a82f2375ce9bd1307afc0b92f
Ben Barber <45568058+bbarberBPL@users.noreply.github.com>
2022-08-23 15:48:33 -0400

    Fix 287 (#288)
    * Fixed bug in Curator::Parsers::InputParser.corp_name_part_splitter (fixes #287)
    
    * Updated some gem dependencies
    
    * Added forward slash to unicode characters in fixtures to ensure they encode properly

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,7 +36,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
   c.configure_rspec_metadata!
   c.hook_into :faraday, :webmock
-  # c.default_cassette_options = { record: :new_episodes }
+  c.default_cassette_options = { record: :new_episodes }
   c.ignore_request do |request|
     request.uri =~ /solr/
   end
