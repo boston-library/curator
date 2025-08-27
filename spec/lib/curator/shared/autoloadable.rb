0efb515eda378afd26d5b333cc57a179fdd79f76
bbarberBPL <bbarber@bpl.org>
2025-08-27 16:13:30 -0400

    Down to 7 failures [skip ci]

--- spec/lib/curator/shared/autoloadable.rb
+++ spec/lib/curator/shared/autoloadable.rb
@@ -4,7 +4,6 @@ RSpec.shared_examples 'autoloadable' do
   it { is_expected.to respond_to(:autoload,
                                  :autoload_at,
                                  :autoload_under,
-                                 :autoloads,
                                  :eager_autoload,
                                  :eager_load!) }
 end
