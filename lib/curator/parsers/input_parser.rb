74eb4b7e2d6860e6f12e9b069e25fdf6a0be8f27
bbarberBPL <bbarber@bpl.org>
2025-09-24 13:04:21 -0400

    Fixes #381
    Added missing  exemplary_image_of_institutions.blank? to clause in exemplary_image_of
    
    Updated travis config

--- lib/curator/parsers/input_parser.rb
+++ lib/curator/parsers/input_parser.rb
@@ -74,7 +74,7 @@ module Curator
 
         value = value.force_encoding('UTF-8')
         value = value.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '') unless value.valid_encoding?
-        HTMLEntities.new.decode(ActionView::Base.full_sanitizer.sanitize(value.gsub(/<br[\s]*\/>/, ' '))).gsub("\\'", "'").squish
+        value.gsub("\\'", "'").squish
       end
 
       ##
