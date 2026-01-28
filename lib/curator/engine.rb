8306c552c1e4d35e668c976822ace0eeb649f627
bbarberBPL <bbarber@bpl.org>
2026-01-28 15:43:29 -0500

    Fixed linting problem, this should be good to go now

--- lib/curator/engine.rb
+++ lib/curator/engine.rb
@@ -74,7 +74,6 @@ module Curator
       end
     end
 
-
     initializer 'curator.append_migrations' do |app|
       unless app.root.to_s.match root.to_s
         config.paths['db/migrate'].expanded.each do |path|
